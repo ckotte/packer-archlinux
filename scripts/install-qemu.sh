360c1f74a4d161f08ddc77dc9feb5b65b6643555
Christian Kotte <christian.kotte@gmx.de>
2020-10-26 18:57:20 +0100

    Fix script name in install-qemu.sh

--- scripts/install-qemu.sh
+++ scripts/install-qemu.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash -x
 
 # https://wiki.archlinux.org/index.php/QEMU#Preparing_an_(Arch)_Linux_guest
-echo ">>>> install-virtualbox.sh: Installing QEMU Guest Agent.."
+echo ">>>> install-qemu.sh: Installing QEMU Guest Agent.."
 /usr/bin/pacman -S --noconfirm qemu-guest-agent
 
-echo ">>>> install-virtualbox.sh: Enabling QEMU Guest Agent service.."
+echo ">>>> install-qemu.sh: Enabling QEMU Guest Agent service.."
 /usr/bin/systemctl enable qemu-ga.service
