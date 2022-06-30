03d5927c4afba664876acc46028f8407073c5e7a
Christian Kotte <christian.kotte@gmx.de>
2022-07-01 00:03:49 +0700

    Use double quotes to prevent globbing and word splitting
    Shellcheck recommendation: shellcheck(SC2086)

--- srv/enable-ssh.sh
+++ srv/enable-ssh.sh
@@ -3,7 +3,7 @@
 PASSWORD=$(/usr/bin/openssl passwd -crypt 'vagrant')
 
 # Vagrant-specific configuration
-/usr/bin/useradd --password ${PASSWORD} --comment 'Vagrant User' --create-home --user-group vagrant
+/usr/bin/useradd --password "${PASSWORD}" --comment 'Vagrant User' --create-home --user-group vagrant
 echo 'Defaults env_keep += "SSH_AUTH_SOCK"' > /etc/sudoers.d/10_vagrant
 echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/10_vagrant
 /usr/bin/chmod 0440 /etc/sudoers.d/10_vagrant
